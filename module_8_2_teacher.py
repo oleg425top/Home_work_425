01834fe2f42ca3aa6bb1312f4cb6ea2d15a7000d
oleg425top <oleg59152@gmail.com>
2024-12-01 14:32:38 +0500

    добавил метод add_mark

--- module_8_2_teacher.py
+++ module_8_2_teacher.py
@@ -17,8 +17,16 @@ class Teacher:
         self.__experience = new_experience
 
     def get_teacher_data(self):
-        return f'Имя: {self.get_name()}\nОбразование: {self.__education}\nОпыт работы: {self.get_experienc()} года'
+        return f'Имя: {self.get_name()}\nОбразование: {self.__education}\nОпыт работы: {self.get_experienc()} года/лет'
+
+    def add_mark(self, name_of_student, mark_of_student):
+        self.name_of_student = name_of_student
+        self.mark_of_student = mark_of_student
+        return f'{self.get_name()} поставил оценку: {self.mark_of_student} студенту: {self.name_of_student}'
 
 
 teacher_1 = Teacher('Иван Петров', 'БГПУ', 4)
+teacher_1.set_experience(8)
+print(teacher_1.add_mark('Вася Пупкин', 5))
+print(teacher_1.get_name())
 print(teacher_1.get_teacher_data())

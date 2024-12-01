6dd7e8d2fd72735919c3bcccc8fad4ec8df9baa8
oleg425top <oleg59152@gmail.com>
2024-12-01 14:05:54 +0500

    добавил гетторы и один сеттор для класса

--- module_8_2_teacher.py
+++ module_8_2_teacher.py
@@ -3,3 +3,15 @@ class Teacher:
         self.__name = name
         self.__education = education
         self.__experience = experience
+
+    def get_name(self):
+        return self.__name
+
+    def get_education(self):
+        return self.__education
+
+    def get_experienc(self):
+        return self.__experience
+
+    def set_experience(self, new_experience):
+        self.__experience = new_experience

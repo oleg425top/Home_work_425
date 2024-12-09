0018d744d54eb2a3045b30cbb4d52344b647ac63
oleg425top <oleg59152@gmail.com>
2024-12-09 15:52:57 +0500

    Добавил блок питания

--- 08_04_classes_PC/08_04_PC.py
+++ 08_04_classes_PC/08_04_PC.py
@@ -1,3 +1,11 @@
+import time
+
+
 class PowerUnit:
     def __int__(self, power):
         self.power = power
+
+    def submit_voltage(self, sub_power:bool):
+        if self.power > 0 :
+            sub_power = True
+            return f'блок питания мощностью {self.power} готов к работе... {time.sleep(2)}...'

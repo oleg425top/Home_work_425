15c4b2726dbf0bdbe2760420e422d2199b938d0a
oleg425top <oleg59152@gmail.com>
2024-12-09 17:13:06 +0500

    Добавил материнку

--- 08_04_classes_PC/08_04_PC.py
+++ 08_04_classes_PC/08_04_PC.py
@@ -1,11 +1,40 @@
-import time
+import time, datetime
 
 
 class PowerUnit:
-    def __int__(self, power):
+    def __init__(self, name, power):
         self.power = power
+        self.name = name
 
-    def submit_voltage(self, sub_power:bool):
-        if self.power > 0 :
+    def __str__(self):
+        return f'Блок питания {self.name} мощностью {self.power} ватт - готов к работе...'
+
+    def submit_voltage(self):
+        sub_power = False
+        if self.power > 0:
             sub_power = True
-            return f'блок питания мощностью {self.power} готов к работе... {time.sleep(2)}...'
+            return sub_power
+        else:
+            return sub_power
+
+
+class Motherboard(PowerUnit):
+    def __init__(self, chipset, name, power):
+        super().__init__(name, power)
+        self.chipset = chipset
+
+    def redistribute_voltage(self):
+        if super().submit_voltage():
+            return f'Chipset:{self.chipset}...\nБлок питания:{self.name}...\nМощность: {self.power} Ватт...\nЗагрузка компонентов...'
+        else:
+            return f'нет питания '
+
+
+print(datetime.date.today(), 'Загрузка....')
+time.sleep(2)
+bloc = PowerUnit('SuperBlock', 300)
+print(bloc)
+time.sleep(2)
+mother = Motherboard('Dazzle_CX', 'SuperBlock', 300)
+print(mother.redistribute_voltage())
+

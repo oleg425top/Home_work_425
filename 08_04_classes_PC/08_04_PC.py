a0da3531e4cdda147e8fef6d46679acaf9152623
oleg425top <oleg59152@gmail.com>
2024-12-10 16:22:07 +0500

    Изменил код под условия из ДЗ

--- 08_04_classes_PC/08_04_PC.py
+++ 08_04_classes_PC/08_04_PC.py
@@ -6,44 +6,34 @@ class PowerUnit:
         self.power = power
         self.name = name
 
-    def __str__(self):
-        return f'  Блок питания {self.name} мощностью {self.power} ватт - готов к работе...'
-
     def submit_voltage(self):
-        sub_power = False
-        if self.power > 0:
-            sub_power = True
-            return sub_power
-        else:
-            return sub_power
+        return f'Подано напряжение...'
+
+    def __str__(self):
+        return f'  Блок питания {self.name} мощностью {self.power} ватт {self.submit_voltage()}- готов к работе...'
 
 
-class Motherboard(PowerUnit):
-    def __init__(self, chipset, name, power):
-        super().__init__(name, power)
+class Motherboard:
+    def __init__(self, chipset):
         self.chipset = chipset
 
     def redistribute_voltage(self):
-        if super().submit_voltage():
-            return f'  Chipset:{self.chipset}...\n  Блок питания:{self.name}...\n  Мощность: {self.power} Ватт...\n  Загрузка компонентов...'
-        else:
-            return f'нет питания '
+        return f'Загрузка компонентов...'
+
+    def __str__(self):
+        return f'  Chipset:{self.chipset}...\n{self.redistribute_voltage()}'
 
 
-class CP(PowerUnit):
-    def __init__(self, clock_frequency, number_of_cores, name, power):
-        super().__init__(name, power)
+class CP:
+    def __init__(self, clock_frequency, number_of_cores):
         self.clock_frequency = clock_frequency
         self.number_of_cores = number_of_cores
 
-    def __str__(self):
-        return f"  Материнская плата: {self.name}\n  Частота: {self.clock_frequency}\n  Количество ядер: {self.number_of_cores}"
-
     def activate_turbo_mode(self):
-        if super().submit_voltage():
-            return f'  Активирован турбо режим...'
-        else:
-            return f'не достаточно питания...'
+        return f'  Активирован турбо режим...'
+
+    def __str__(self):
+        return f"Частота: {self.clock_frequency}\n  Количество ядер: {self.number_of_cores}\n{self.activate_turbo_mode()}"
 
 
 class RAM:
@@ -74,6 +64,7 @@ class SSD:
     def __str__(self):
         return f'SSD: {self.volume} TB\n  {self.safe_data()}\n  {self.delete_data()}'
 
+
 class VideoCard:
     def __init__(self, model, memory_capacity):
         self.model = model
@@ -86,57 +77,24 @@ class VideoCard:
         return f'VideoCard: {self.model}\n  Memory: {self.memory_capacity} GB\n  {self.display_on_screen()}'
 
 
+class Computer(PowerUnit, Motherboard, CP, RAM, SSD, VideoCard):
+    def __init__(self, mark, name, power, chipset, clock_frequency, number_of_cores, memory_capacity, frequency_memory, volume, model, video_memory_capacity):
+        self.mark = mark
+        PowerUnit.__init__(self, name, power)
+        Motherboard.__init__(self, chipset)
+        CP.__init__(self, clock_frequency, number_of_cores)
+        RAM.__init__(self, memory_capacity, frequency_memory)
+        SSD.__init__(self, volume)
+        VideoCard.__init__(self, model, memory_capacity)
 
-print(datetime.date.today())
-for l in 'Загрузка...':
-    print(l, end='')
-    time.sleep(0.2)
-time.sleep(2)
-print()
-bloc = PowerUnit('SuperBlock', 300)
-print(bloc)
-time.sleep(2)
-for l in 'Загрузка...':
-    print(l, end='')
-    time.sleep(0.2)
-time.sleep(2)
-print()
-mother = Motherboard('Dazzle_CX', 'SuperBlock', 300)
-print(mother.redistribute_voltage())
-time.sleep(2)
-for l in 'Загрузка...':
-    print(l, end='')
-    time.sleep(0.2)
-time.sleep(2)
-print()
-cp = CP(1400, 8, 'AMD', 400)
-print(cp)
-time.sleep(2)
-print(cp.activate_turbo_mode())
-for l in 'Загрузка...':
-    print(l, end='')
-    time.sleep(0.2)
-time.sleep(2)
-print()
-ram = RAM(8, 2400)
-print(ram)
-for l in 'Загрузка...':
-    print(l, end='')
-    time.sleep(0.2)
-time.sleep(2)
-print()
-ssd = SSD(4)
-print(ssd)
-for l in 'Загрузка...':
-    print(l, end='')
-    time.sleep(0.2)
-time.sleep(2)
-print()
-video_card = VideoCard('Radeon 9200-SE', 32 )
-print(video_card)
-for l in '   Добро пожаловать!':
-    print(l, end='')
-    time.sleep(0.2)
+    def __str__(self):
+        return f'{PowerUnit.__str__(self)}\n{Motherboard.__str__(self)}\n{CP.__str__(self)}\n{RAM.__str__(self)}\n{SSD.__str__(self)}\n{VideoCard.__str__(self)}'
 
 
+comp = Computer('Pentium III', 'SuperBlock', 300, 'Dazzle_CX', 1400, 16, 16, 2400, 8, 'Radon 9200-SE', 32)
+for l in 'Loading...':
+    print(l, end='')
+    time.sleep(0.22)
+print()
+print(comp)
 

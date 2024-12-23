d18b1383ec5696ff09fe74e49c553318b42017a5
oleg425top <oleg59152@gmail.com>
2024-12-23 11:31:07 +0500

    Add tests for all methods!!!

--- Stack_Unittest/test_stack_unittest/test_stack_unittest.py
+++ Stack_Unittest/test_stack_unittest/test_stack_unittest.py
@@ -2,12 +2,13 @@ import unittest
 
 from stack import Node, Stack
 
+
 class TestNode(unittest.TestCase):
     test_node = Node('test_data')
 
-
     def test_01_node_init(self):
         self.assertEqual(TestNode.test_node.data, 'test_data')
+
 
 class TestStack(unittest.TestCase):
     test_stack = Stack(2)
@@ -16,41 +17,38 @@ class TestStack(unittest.TestCase):
         self.assertEqual(TestStack.test_stack.stack_size, 2)
 
     def test_03_push(self):
-        self.assertEqual(TestStack.test_stack.push(10), None)
+        self.assertEqual(TestStack.test_stack.push(10.5), None)
         self.assertEqual(TestStack.test_stack.push(11), None)
         self.assertEqual(TestStack.test_stack.push(12), 'Стек переполнен')
 
-
-    def test_03_get_data(self):
-        self.assertEqual(TestStack.test_stack.get_data(0), 'Out of range')
-
-
+    def test_04_get_data(self):
+        self.assertEqual(TestStack.test_stack.get_data(0), 11)
 
     def test_05_is_full(self):
         self.assertEqual(TestStack.test_stack.is_full(), True)
 
-    def test_06_pop(self):
+    def test_06_size_stack(self):
+        self.assertEqual(TestStack.test_stack.size_stack(), 2)
+
+    def test_07_counter_int(self):
+        self.assertEqual(TestStack.test_stack.counter_int(), 1)
+
+    def test_08_pop(self):
         self.assertEqual(TestStack.test_stack.pop(), 11)
-        self.assertEqual(TestStack.test_stack.pop(), 10)
+        self.assertEqual(TestStack.test_stack.pop(), 10.5)
         self.assertEqual(TestStack.test_stack.pop(), 'Стек пуст')
 
-    def test_07_is_full(self):
+    def test_09_is_full(self):
         self.assertEqual(TestStack.test_stack.is_full(), False)
 
-
-    def test_08_is_empty(self):
+    def test_10_is_empty(self):
         self.assertEqual(TestStack.test_stack.is_empty(), True)
 
-    def test_09_clear_stack(self):
+    def test_11_clear_stack(self):
         self.assertEqual(TestStack.test_stack.clear_stack(), None)
         self.assertEqual(TestStack.test_stack.is_empty(), True)
 
-    def test_10_get_data(self):
+    def test_12_get_data(self):
         self.assertEqual(TestStack.test_stack.get_data(1), 'Out of range')
-
-
-
-
-
 
 

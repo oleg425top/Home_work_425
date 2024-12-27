ed8ae0526c271101307dda07251233844a61a4ee
oleg425top <oleg59152@gmail.com>
2024-12-27 22:32:03 +0500

    есть ошибки

--- Queue_Unittest/main_queue.py
+++ Queue_Unittest/main_queue.py
@@ -10,31 +10,30 @@ def choice(number, queue):
             print('очередь пуста')
         else:
             print('очередь не пуста')
-    elif number == 2:
+    if number == 2:
         result_2 = queue.is_full()
-        print(result_2)
         if result_2:
             print('очередь заполнена')
         else:
             print('очередь не заполнена')
-    elif number == 3:
-        if queue.is_full():
-            print('очередь заполнена')
+    if number == 3:
+        full = queue.is_full()
+        if full:
+            print('очередь уже заполнена')
             return
         else:
-            return queue.enqueue(input('введите данные: '))
-    elif number == 4:
+
+            return queue.enqueue(input('введите данные: ')), print('данные введены')
+    if number == 4:
         return queue.dequeue()
-    elif number == 5:
+    if number == 5:
         return queue.show_queue()
-    else:
-        return 'ваше число не верно'
 
 
 user_choice = int(input(
     'Сделайте ваш выбор:\n1: IsEmpty — проверка очереди на пустоту;\n2: IsFull — проверка очереди на заполнение;\n3: Enqueue — добавление элемента в очередь;\n4: Dequeue — удаление элемента из очереди;\n5: Show — отображение всех элементов очереди на экран;\n0: Выход!\n:'))
-while user_choice != 0:
+while user_choice != 0 and user_choice <= 5:
     choice(user_choice, main_queue)
     user_choice = int(input('\nваш выбор: '))
 else:
-    print('До свидания ')
+    print('До свидания/ вы ввели некорректные данные ')

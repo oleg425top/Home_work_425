be49dbef2bd0955e60cf6da5f57c8ebb784eff15
oleg425top <oleg59152@gmail.com>
2025-01-04 14:24:52 +0500

    добавил метод adddd!!!

--- Pickle/Pickle_music_groups.py
+++ Pickle/Pickle_music_groups.py
@@ -8,3 +8,12 @@ def load_from_file(name_of_file):
         return 'File Not Found'
     return f'Данные: {load_data} получены из файла {name_of_file}'
 
+class MusicGroupsDict:
+    def __init__(self):
+        self.music_dict = {}
+
+    def add_group(self, group, *albums):
+        if group in self.music_dict.keys():
+            self.music_dict[group].append(albums)
+        else:
+            self.music_dict.update({group: albums})

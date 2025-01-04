7114891dcaeab8dbec38334f0cab0238f0da5b9d
oleg425top <oleg59152@gmail.com>
2025-01-04 14:01:05 +0500

    1 задание готово

--- Pickle/Pickle_Home_work.py
+++ Pickle/Pickle_Home_work.py
@@ -1,3 +1,16 @@
+import pickle
+from pickle import HIGHEST_PROTOCOL
+
+
+def load_from_file(name_of_file):
+    try:
+        with open(name_of_file,'rb') as fp:
+            load_data = pickle.load(fp)
+    except FileNotFoundError:
+        return 'File Not Found'
+    return f'Данные: {load_data} получены из файла {name_of_file}'
+
+
 class CountriesDict:
     def __init__(self):
         self.countries_dict = {}
@@ -32,6 +45,11 @@ class CountriesDict:
     def show(self):
         return f'словарь: {self.countries_dict}'
 
+    def save_to_file(self, name_of_file):
+        with open(f'{name_of_file}.city', 'wb') as fp:
+            pickle.dump(self.countries_dict, fp, HIGHEST_PROTOCOL)
+        return f'данные записаны в файл {name_of_file}.city'
+
 
 if __name__ == '__main__':
     country_dict = CountriesDict()
@@ -49,3 +67,5 @@ if __name__ == '__main__':
     print(country_dict.show())
     print(country_dict.editing('Egypt', 'Khair'))
     print(country_dict.show())
+    print(country_dict.save_to_file('my_countries'))
+    print(load_from_file('my_countries.city'))

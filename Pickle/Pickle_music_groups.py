835fbe9f74b8eb07b19e959eb0630ef6f30e56f9
oleg425top <oleg59152@gmail.com>
2025-01-04 23:25:33 +0500

    добавил метод поиска

--- Pickle/Pickle_music_groups.py
+++ Pickle/Pickle_music_groups.py
@@ -1,12 +1,14 @@
 import pickle
 
+
 def load_from_file(name_of_file):
     try:
-        with open(name_of_file,'rb') as fp:
+        with open(name_of_file, 'rb') as fp:
             load_data = pickle.load(fp)
     except FileNotFoundError:
         return 'File Not Found'
     return f'Данные: {load_data} получены из файла {name_of_file}'
+
 
 class MusicGroupsDict:
     def __init__(self):
@@ -18,6 +20,7 @@ class MusicGroupsDict:
         else:
             self.music_dict.update({groups: list(albums)})
         return f'{groups}, {albums} :данные добавлены'
+
     def show(self):
         return self.music_dict
 
@@ -25,26 +28,39 @@ class MusicGroupsDict:
         for keys, values in self.music_dict.items():
             if name_of_group == keys:
                 self.music_dict.pop(keys)
-                return f'Группа {name_of_group}, c альбомами {values} удалена'
+                return f'Группа {name_of_group}, c альбомами {values[0], values[1]}..... удалена'
         else:
             return f'{name_of_group} :данные для удаления не найдены!!'
 
+    def search(self, data):
+        for keys, values in self.music_dict.items():
+            if data == keys:
+                return f'группа {keys} найдена, её альбомы {values}'
+            else:
+                for key, values in self.music_dict.items():
+                    if data in values:
+                        return f'Альбом: {data} найден в группе {key}'
+        return f'{data}  :данные не найдены!!'
+
+
 if __name__ == '__main__':
-    group =MusicGroupsDict()
-    group.add_group('The Beatles', 'Please Please Me (1963)','A Hard Days Night (1964)','Help! (1965)','Rubber Soul (1965)Revolver (1966)','Sgt. Peppers Lonely Hearts Club Band (1967)','The Beatles (The White Album) (1968)','Abbey Road (1969)')
+    group = MusicGroupsDict()
+    group.add_group('The Beatles', 'Please Please Me (1963)', 'A Hard Days Night (1964)', 'Help! (1965)',
+                    'Rubber Soul (1965)Revolver (1966)', 'Sgt. Peppers Lonely Hearts Club Band (1967)',
+                    'The Beatles (The White Album) (1968)', 'Abbey Road (1969)')
     # print(group.show())
-    group.add_group('The Rolling Stones','The Rolling Stones (1964)', 'Aftermath (1966)', 'Beggars Banquet (1968)', 'Let It Bleed (1969)', 'Sticky Fingers (1971)', 'Exile on Main St. (1972)', 'Some Girls (1978)')
+    group.add_group('The Rolling Stones', 'The Rolling Stones (1964)', 'Aftermath (1966)', 'Beggars Banquet (1968)',
+                    'Let It Bleed (1969)', 'Sticky Fingers (1971)', 'Exile on Main St. (1972)', 'Some Girls (1978)')
     # print(group.show())
-    #'Let It Be (1970)'
+    # 'Let It Be (1970)'
     group.add_group('The Beatles', 'Let It Be (1970)')
     # print(group.show())
-    group.add_group('Pink Floyd', 'The Piper at the Gates of Dawn (1967)','The Dark Side of the Moon (1973)', 'Wish You Were Here (1975)', 'Animals (1977)', 'The Wall (1979)')
+    group.add_group('Pink Floyd', 'The Piper at the Gates of Dawn (1967)', 'The Dark Side of the Moon (1973)',
+                    'Wish You Were Here (1975)', 'Animals (1977)', 'The Wall (1979)')
     print(group.show())
     print(group.delete_data('Pink Floyd'))
     print(group.delete_data('Pinks Floyd'))
     print(group.show())
-
-
-
-
-
+    print(group.search('A Hard Days Night (1964)'))
+    print(group.search('A Hard Days Night (1965)'))
+    print(group.search('The Rolling Stones'))

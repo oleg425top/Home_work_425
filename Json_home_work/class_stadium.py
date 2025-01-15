627b49318f842c0b73d26d79f50a16a463bcb1bf
oleg425top <oleg59152@gmail.com>
2025-01-15 16:48:16 +0500

    со стадионом покончено!!

--- Json_home_work/class_stadium.py
+++ Json_home_work/class_stadium.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Stadium:
     def __init__(self, name, capacity, year_opened):
         self.name = name
@@ -5,8 +8,44 @@ class Stadium:
         self.year_opened = year_opened
 
     def display_info(self):
-        return f"Название: {self.name}, Вместимость: {self.capacity}, Год открытия: {self.year_opened}"
+        return f"Name: {self.name}, Capacity: {self.capacity}, Year opened: {self.year_opened}"
 
     def change_capacity(self, new_capacity):
         self.capacity = new_capacity
-        return f"Вместимость стадиона увеличена до:  {self.capacity} человек"
+        return f"Capacity has been updated to:  {self.capacity} peoples"
+
+
+class JSONStadiumAdapter:
+    @staticmethod
+    def to_json(obj):
+        if isinstance(obj, Stadium):
+            return json.dumps({
+                'name: ': obj.name,
+                'capacity: ': obj.capacity,
+                'year opened: ': obj.year_opened,
+                '<Class>: ': obj.__class__.__name__,
+                '<Methods>: ': {
+                    'display_info': obj.display_info(),
+                    'change_capacity: ': obj.change_capacity(20000)
+                }
+
+            })
+
+    @staticmethod
+    def from_json(obj):
+        obj = json.loads(obj)
+
+        try :
+            result = Stadium(obj['name: '], obj['capacity: '], obj['year opened: '])
+            return result
+        except AttributeError:
+            print('неверная структура')
+
+if __name__ == '__main__':
+    my_stadium = Stadium("Wembley Stadium", 90000, 2007)
+    json_stadium = JSONStadiumAdapter.to_json(my_stadium)
+    print(json_stadium)
+    python_stadium = JSONStadiumAdapter.from_json(json_stadium)
+    print(python_stadium.display_info())
+    print(python_stadium.change_capacity(120000))
+    print(python_stadium.display_info())
